fed4daaae727a5f51e1bc2355f6d3a67ec70c976
a3ad2922709700 <a3ad2922709700@p3nlwpsftp005.shr.prod.phx3.secureserver.net>
2016-03-30 12:08:08 -0700

    some pathways fix

--- view-killer.php
+++ view-killer.php
@@ -9,7 +9,7 @@ function rwpse162680_enqueue_script( $hook ) {
         return;
     }
     // wp_enqueue_script( 'avada-child-stylesheet', get_template_directory_uri() . '-Child-Theme/myscript.js' );
-    wp_enqueue_script( 'avada-child-stylesheet', plugins_url( 'view-killer/myscript.js' ) );
+    wp_enqueue_script( 'avada-child-stylesheet', plugins_url( 'wp_view_killer/myscript.js' ) );
     // wp_enqueue_script( plugin_dir_path(rad) . 'view-killer/myscript.js' );
     // wp_enqueue_script( plugins_url( 'myscript.js' , __FILE__ ) );
 }

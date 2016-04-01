588d3d66ebd0e912e8f4843a3125d9baebf3646b
zackn9ne <zackn9ne@gmail.com>
2016-04-01 16:24:24 -0400

    conditional running of scripts and clean up yata yata

--- myscript.js
+++ myscript.js
@@ -3,12 +3,34 @@ $j=jQuery.noConflict();
 
 $j(document).ready(function() {
 
+
+
+
+window.onload = function () { 
+
+
+
 $j(".switch-tmce").css({'color' : 'lightgray', 'cursor' : 'not-allowed'});
+// $j(".wp-editor-tabs").css({'display' : 'none'});
+$j(".quicktags-toolbar").children().not(".ed_button[value='link']").hide();
+// $j(.ed_button[value='link']).hide();
 
 
-});
+
+// $j(".quicktags-toolbar").children().not("#qt_w2dc-field-input-99_link").hide();
 
 
-jQuery(document).ready(function() {
-    document.getElementById("content-tmce").onclick = 'none';
+// jQuery(document).ready(function() {
+
+
+	// alert("It's loaded!");
+    $j(".switch-tmce").click(false);
+
+	// $j(".quicktags-toolbar").children().not(input[value='link']).hide();
+	// $j(".quicktags-toolbar").children().not([value='link']).hide();
+
+
+};
+
+
 });
--- view-killer.php
+++ view-killer.php
@@ -3,14 +3,32 @@
 Plugin Name: View Killer
 */
 
-function rwpse162680_enqueue_script( $hook ) {
-    $screen = get_current_screen();
+function where_are_we( $hook ) {
+
     if ( $hook == 'post.php' && $screen->post_type != 'w2dc_listing' ) {
-        return;
+       //If You're In The Right Place Alert this notice
+       echo 'you are on a w2dc_listing';
+       
+       //If You're In The Right Place Run This Bunch Of JavaScript
+       wp_enqueue_script( 'avada-child-stylesheet', plugins_url( 'view-killer/myscript.js' ) );
+       
+       // If You're In The Right Place Convert Line Breaks To BR's
+        function amytheme_tinymce_settings( $tinymce_init_settings ) {
+            $tinymce_init_settings['forced_root_block'] = false;
+            return $tinymce_init_settings;
+        }
+        
+        add_filter( 'tiny_mce_before_init', 'amytheme_tinymce_settings' );
+        remove_filter ( 'the_content', 'wpautop' );
+        add_filter ( 'the_content', 'add_newlines_to_post_content' );
+        
+        function add_newlines_to_post_contentz( $content ) {
+            return nl2br( $content );
+        }
+
+    } else {
+    //Page die
     }
-    // wp_enqueue_script( 'avada-child-stylesheet', get_template_directory_uri() . '-Child-Theme/myscript.js' );
-    wp_enqueue_script( 'avada-child-stylesheet', plugins_url( 'view-killer/myscript.js' ) );
-    // wp_enqueue_script( plugin_dir_path(rad) . 'view-killer/myscript.js' );
-    // wp_enqueue_script( plugins_url( 'myscript.js' , __FILE__ ) );
+
 }
-add_action( 'admin_enqueue_scripts', 'rwpse162680_enqueue_script' );
+add_action( 'admin_enqueue_scripts', 'where_are_we' );
